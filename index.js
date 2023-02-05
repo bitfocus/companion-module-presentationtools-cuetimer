91a44c25effdd5008d1a371f21dc103033ae06d6
Mohammad Ahmad <eng.ma90@gmail.com>
2023-02-05 19:23:04 +0200

    Removed old variable sender
    the this.system was used and it is no longer supported in v3

--- index.js
+++ index.js
@@ -40,7 +40,6 @@ updateConfig(config) {
 		self.initTCP()
 	}
 
-	self.setupEventListeners.bind(self)();
 }
 
 async init(config) {
@@ -49,14 +48,8 @@ async init(config) {
 	self.config = config;
 
 
-	self.variable1Value = '';
-	self.variable2Value = '';
-	self.variable3Value = '';
-	self.variable4Value = '';
-	self.variable5Value = '';
 
 	self.initTCP()
-	self.setupEventListeners.bind(this)();
 	self.actions()
 	self.feedbacks()
 	self.presets()
@@ -170,28 +163,6 @@ initTCP() {
 getConfigFields() {
 	var self = this
 
-	const dynamicVariableChoices = [];
-	dynamicVariableChoices.push({
-		id: null,
-		label: '(No Variable Selected)'
-	});
-
-	// TODO: Find an alternative to this.system in v3
-	try {
-		this.system.emit('variable_get_definitions', (definitions) =>
-			Object.entries(definitions).forEach(([instanceLabel, variables]) =>
-				variables.forEach((variable) =>
-					dynamicVariableChoices.push({
-						id: `${instanceLabel}:${variable.name}`,
-						label: `${instanceLabel}:${variable.name}`
-					})
-				)
-			)
-		);
-	}
-	catch(error) {
-		this.log('error', 'Error with Variables: ' + error.toString());
-	}
 
 	return [
 		{
@@ -217,217 +188,9 @@ getConfigFields() {
 			width: 6,
 			regex: Regex.PORT,
 		},
-		{
-			type: 'static-text',
-			id: 'variablesinfo',
-			label: 'Send Companion Variables to CueTimer',
-			width: 12,
-			value: `
-				<div class="alert alert-warning">
-					<div>
-						<strong>You can select up to 5 Companion Variables to send to CueTimer. They will be sent any time their values change within Companion.</strong>
-					</div>
-				</div>
-			`
-		},
-		{
-			type: 'dropdown',
-			id: 'variable1',
-			label: 'Variable 1',
-			width: 6,
-			tooltip: 'Variable to send to CueTimer',
-			choices: dynamicVariableChoices,
-			minChoicesForSearch: 5
-		},
-		{
-			type: 'dropdown',
-			id: 'variable2',
-			label: 'Variable 2',
-			width: 6,
-			tooltip: 'Variable to send to CueTimer',
-			choices: dynamicVariableChoices,
-			minChoicesForSearch: 5
-		},
-		{
-			type: 'dropdown',
-			id: 'variable3',
-			label: 'Variable 3',
-			width: 6,
-			tooltip: 'Variable to send to CueTimer',
-			choices: dynamicVariableChoices,
-			minChoicesForSearch: 5
-		},
-		{
-			type: 'dropdown',
-			id: 'variable4',
-			label: 'Variable 4',
-			width: 6,
-			tooltip: 'Variable to send to CueTimer',
-			choices: dynamicVariableChoices,
-			minChoicesForSearch: 5
-		},
-		{
-			type: 'dropdown',
-			id: 'variable5',
-			label: 'Variable 5',
-			width: 6,
-			tooltip: 'Variable to send to CueTimer',
-			choices: dynamicVariableChoices,
-			minChoicesForSearch: 5
-		},
 	]
 }
 
-variableListener(variables) {
-	try {
-		let dataChanged = false;
-
-		for (let key in variables) {
-			if (variables.hasOwnProperty(key)) {
-				if (key == this.config.variable1) {
-					if (this.variable1Value !== variables[key]) {
-						dataChanged = true;
-					}
-					this.variable1Value = variables[key];
-				}
-	
-				if (key == this.config.variable2) {
-					if (this.variable2Value !== variables[key]) {
-						dataChanged = true;
-					}
-					this.variable2Value = variables[key];
-				}
-	
-				if (key == this.config.variable3) {
-					if (this.variable3Value !== variables[key]) {
-						dataChanged = true;
-					}
-					this.variable3Value = variables[key];
-				}
-	
-				if (key == this.config.variable4) {
-					if (this.variable4Value !== variables[key]) {
-						dataChanged = true;
-					}
-					this.variable4Value = variables[key];
-				}
-	
-				if (key == this.config.variable5) {
-					if (this.variable5Value !== variables[key]) {
-						dataChanged = true;
-					}
-					this.variable5Value = variables[key];
-				}
-			}
-		}
-		
-		if (dataChanged) {
-			this.sendVariables.bind(this)();
-		}
-	}
-	catch(error) {
-		this.log('error', 'Error sending variables: ' + error.toString())
-	}
-};
-
-sendVariables() {
-	let variableSendArray = [];
-		
-	if (this.config.variable1) {
-		variableSendArray.push({
-			variable: this.config.variable1,
-			value: this.variable1Value
-		});
-	}
-
-	if (this.config.variable2) {
-		variableSendArray.push({
-			variable: this.config.variable2,
-			value: this.variable2Value
-		});
-	}
-
-	if (this.config.variable3) {
-		variableSendArray.push({
-			variable: this.config.variable3,
-			value: this.variable3Value
-		});
-	}
-
-	if (this.config.variable4) {
-		variableSendArray.push({
-			variable: this.config.variable4,
-			value: this.variable4Value
-		});
-	}
-		
-	if (this.config.variable5) {
-		variableSendArray.push({
-			variable: this.config.variable5,
-			value: this.variable5Value
-		});
-	}
-
-	if (variableSendArray.length > 0) {
-		let variableSendString = '$VARIABLES:' + JSON.stringify(variableSendArray) + '\r\n';
-		if (this.socket !== undefined && this.socket.isConnected) {
-			this.socket.send(variableSendString);
-		}
-	}
-}
-
-setupEventListeners() {
-	// TODO: Find an alternative to this.system in v3
-
-	//get initial values
-
-	try {
-		if (this.config.variable1) {
-			this.system.emit('variable_parse', '$(' + this.config.variable1 + ')', (parsedValue) => {
-				this.variable1Value = parsedValue;
-			});
-		}
-	
-		if (this.config.variable2) {
-			this.system.emit('variable_parse', '$(' + this.config.variable2 + ')', (parsedValue) => {
-				this.variable2Value = parsedValue;
-			});
-		}
-	
-		if (this.config.variable3) {
-			this.system.emit('variable_parse', '$(' + this.config.variable3 + ')', (parsedValue) => {
-				this.variable3Value = parsedValue;
-			});
-		}
-	
-		if (this.config.variable4) {
-			this.system.emit('variable_parse', '$(' + this.config.variable4 + ')', (parsedValue) => {
-				this.variable4Value = parsedValue;
-			});
-		}
-	
-		if (this.config.variable5) {
-			this.system.emit('variable_parse', '$(' + this.config.variable5 + ')', (parsedValue) => {
-				this.variable5Value = parsedValue;
-			});
-		}
-
-		this.sendVariables();
-	
-		//get values any time they change
-		if (this.activeVariableListener) {
-			this.system.removeListener('variables_changed', this.activeVariableListener);
-			delete this.activeVariableListener;
-		}
-		
-		this.activeVariableListener = this.variableListener.bind(this);
-		this.system.on('variables_changed', this.activeVariableListener);
-	}
-	catch(error) {
-		this.log('error', 'Error with sending variables: ' + error.toString());
-	}
-	
-};
 
 actions() {
 
