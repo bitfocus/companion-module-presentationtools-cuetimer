fb04982b0471a8116ad07f6073089cec8203e2a8
Mohammad Ahmad <eng.ma90@gmail.com>
2023-03-19 19:44:56 +0200

    Made updateConfig async

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ class CueTimerInstance extends InstanceBase {
 			: null
 	}
 
-	updateConfig(config) {
+	async updateConfig(config) {
 		var self = this
 		self.config = config
 		var resetConnection = false
